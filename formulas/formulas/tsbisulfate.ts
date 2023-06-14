b3787745ef86770bfdc7c90ceff1b4bcab045c8b
John Gazzini <gazzini.john@gmail.com>
2023-06-13 20:21:04 -0500

    test cleanup

--- formulas/formulas/tsbisulfate.ts
+++ formulas/formulas/tsbisulfate.ts
@@ -1,43 +1,42 @@
-import { Formula } from "../models/Formula";
-import { DT } from "../models/Helpers";
-import { ch } from "../readings/ch";
-import { cya } from "../readings/cya";
-import { fc } from "../readings/fc";
-import { ph } from "../readings/ph";
-import { ta } from "../readings/ta";
-import { tc } from "../readings/tc";
-import { baking_soda } from "../treatments/baking_soda";
-import { cal_chlor } from "../treatments/cal_chlor";
-import { calc_hypo } from "../treatments/calc_hypo";
-import { m_acid } from "../treatments/m_acid";
+import { Formula } from '~/formulas/models/Formula';
+import { DT } from '~/formulas/models/Helpers';
+import { ch } from '~/formulas/readings/ch';
+import { cya } from '~/formulas/readings/cya';
+import { fc } from '~/formulas/readings/fc';
+import { ph } from '~/formulas/readings/ph';
+import { ta } from '~/formulas/readings/ta';
+import { tc } from '~/formulas/readings/tc';
+import { baking_soda } from '~/formulas/treatments/baking_soda';
+import { cal_chlor } from '~/formulas/treatments/cal_chlor';
+import { calc_hypo } from '~/formulas/treatments/calc_hypo';
+import { m_acid } from '~/formulas/treatments/m_acid';
 import { cya as cya_treatment } from '~/formulas/treatments/cya';
-import { soda_ash } from "../treatments/soda_ash";
-import { sodium_bisulfate } from "../treatments/sodium_bisulfate";
-import { lsi } from "../treatments/lsi";
-
+import { soda_ash } from '~/formulas/treatments/soda_ash';
+import { sodium_bisulfate } from '~/formulas/treatments/sodium_bisulfate';
+import { lsi } from '~/formulas/treatments/lsi';
 
 
 export const testSodiumBisulfateFormula: Formula = {
-  name: '(Test) Sodium Bisulfate',
-  description: 'Testing Only (for now): Targeted at salt & chlorine pools, this plan takes 6 readings & uses calcium hypochlorite as the sanitizer, with the added option of Sodium Bisulfate to lower pH.',
-  id: 'tsbisulfate',
-  readings: [
-    fc,
-    tc,
-    ph,
-    ta,
-    ch,
-    cya,
-  ],
-  targets: [],
-  adjusters: [],
-  balanceOrder: [
-    DT('fc', calc_hypo, null),
-    DT('ph', soda_ash, sodium_bisulfate),
-    DT('ta', baking_soda, m_acid),
-    DT('ch', cal_chlor, null),
-    DT('cya', cya_treatment, null),
-  ],
-  //put lsi here? 
-  alwaysCheck: [lsi],
-}
+    name: '(Test) Sodium Bisulfate',
+    description: 'Testing Only (for now): Targeted at salt & chlorine pools, this plan takes 6 readings & uses calcium hypochlorite as the sanitizer, with the added option of Sodium Bisulfate to lower pH.',
+    id: 'tsbisulfate',
+    readings: [
+        fc,
+        tc,
+        ph,
+        ta,
+        ch,
+        cya,
+    ],
+    targets: [],
+    adjusters: [],
+    balanceOrder: [
+        DT('fc', calc_hypo, null),
+        DT('ph', soda_ash, sodium_bisulfate),
+        DT('ta', baking_soda, m_acid),
+        DT('ch', cal_chlor, null),
+        DT('cya', cya_treatment, null),
+    ],
+    //put lsi here? 
+    alwaysCheck: [lsi],
+};
---
+++ tests/formulas/sodiumBisulfate.test.ts
@@ -0,0 +1,100 @@
+import { calculate } from '~/formulas/calculator';
+import { ReadingValues } from '~/formulas/models/misc/Values';
+import { EffectiveTargetRange } from '~/formulas/models/TargetRange';
+import { getPool } from '../helpers/testHelpers';
+import { testSodiumBisulfateFormula } from '~/formulas/formulas/tsbisulfate';
+
+/*  This should be the formula under test  */
+const formula = testSodiumBisulfateFormula;
+
+describe('Sodium Bisulfate Formula', () => {
+    it('returns all the treatments to raise levels when they\'re low', () => {
+        // Arrange
+        const pool = getPool();
+        const targetLevels: EffectiveTargetRange[] = [];
+        const readings: ReadingValues = {
+            fc: 0,
+            ph: 0,
+            tc: 0,
+            ta: 0,
+            ch: 0,
+            cya: 0,
+            temp_f: 80,
+            tds: 0,
+        };
+
+        // Act
+        const res = calculate({
+            formula,
+            pool,
+            readings,
+            targetLevels,
+            substitutions: {},
+        });
+
+        // Assert
+        expect(Object.keys(res).length).toBe(5);
+        expect(res.calc_hypo).toBeCloseTo(8.32);
+        expect(res.soda_ash).toBeCloseTo(48.0);
+        expect(res.baking_soda).toBeCloseTo(147.2);
+        expect(res.cal_chlor).toBeCloseTo(432);
+        expect(res.cya).toBeCloseTo(52);
+    });
+
+    it('returns all the treatments to lower levels when they\'re high', () => {
+        // Arrange
+        const pool = getPool();
+        const targetLevels: EffectiveTargetRange[] = [];
+        const readings: ReadingValues = {
+            bro: 9,
+            ph: 8,
+            ta: 300,
+            tc: 9,
+            cya: 200,
+            temp_f: 200,
+            tds: 10000,
+        };
+
+        // Act
+        const res = calculate({
+            formula,
+            pool,
+            readings,
+            targetLevels,
+            substitutions: {},
+        });
+
+        // Assert
+        expect(Object.keys(res).length).toBe(1);
+        expect(res.sb).toBeCloseTo(48.84);
+    });
+
+    it('doesn\'t recommend anything when levels are balanced', () => {
+        // Arrange
+        const pool = getPool();
+        const targetLevels: EffectiveTargetRange[] = [];
+        const readings: ReadingValues = {
+            bro: 3,
+            ph: 7.2,
+            tc: 3,
+            ta: 200,
+            ch: 300,
+            cya: 40,
+            temp_f: 80,
+            tds: 500
+        };
+
+        // Act
+        const res = calculate({
+            formula,
+            pool,
+            readings,
+            targetLevels,
+            substitutions: {},
+        });
+
+        // Assert
+        expect(Object.keys(res).length).toBe(1);
+        expect(res.lsi).toBeCloseTo(0.052);
+    });
+});
